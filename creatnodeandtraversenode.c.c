ba7926cbcbc14d910a0a27d4f2a0d084f7ea46a1
yingma123 <113251885+yingma123@users.noreply.github.com>
2023-04-06 10:59:53 +0800

    firstgit

---
+++ creatnodeandtraversenode.c.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main (){
+    struct node{
+        char a[20];
+        struct node *next;
+    }*head,*tail;
+    head=tail=NULL;
+    while(1){
+        struct node *temp=(struct node *)malloc(sizeof(struct node));
+        temp->next=NULL;
+        scanf("%s",temp->a);
+        if(temp->a[0]=='0'){
+            break;
+        }
+        else{
+            if(head==NULL){
+                head=tail=temp;
+                continue;
+            }
+            else{
+                tail->next=temp;
+                tail=temp;
+                
+            }
+        }
+        free(temp);
+    
+     
+
+    }
+    struct node *current=head;
+    while(current!=NULL){
+        printf("%s ",current->a);
+        current=current->next;
+    }
+ //what are you fucking doing
+}
